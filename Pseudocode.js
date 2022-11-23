e389279d0fc97e8c77229be78967fa673c38139f
kmartyn30 <107796290+kmartyn30@users.noreply.github.com>
2022-11-23 14:37:47 -0500

    Add files via upload

---
+++ Pseudocode.js
@@ -0,0 +1,11 @@
+Pseudocode
+// if-statement syntax
+if ( condition ) { code to execute }
+// pseudocode practice
+if (a date is entered) {
+    Filter the default data to show only the date entered
+};
+
+if (date) {
+    filteredData = filteredData.filter(row => row.datetime === date);
+  };
